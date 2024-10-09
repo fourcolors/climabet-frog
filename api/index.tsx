40cb36ea4eb4c8ec09374eaf6b3d76208cfcf2bc
fourcolors <sterlingcobb@gmail.com>
2024-10-08 20:57:14 -0600

    add transaction buttons

--- api/index.tsx
+++ api/index.tsx
@@ -1,8 +1,13 @@
-import { Button, Frog, TextInput } from 'frog'
-import { devtools } from 'frog/dev'
-import { serveStatic } from 'frog/serve-static'
+import { Button, Frog, parseEther } from "frog";
+import { createSystem } from "frog/ui";
+import { devtools } from "frog/dev";
+import { serveStatic } from "frog/serve-static";
 // import { neynar } from 'frog/hubs'
-import { handle } from 'frog/vercel'
+import { handle } from "frog/vercel";
+import { rootstock, rootstockTestnet } from "viem/chains";
+import placeBetABI from "../placeBetABI.js";
+
+const { Image } = createSystem();
 
 // Uncomment to use Edge Runtime.
 // export const config = {
@@ -10,67 +15,102 @@ import { handle } from 'frog/vercel'
 // }
 
 export const app = new Frog({
-  assetsPath: '/',
-  basePath: '/api',
+  assetsPath: "/",
+  basePath: "/api",
   // Supply a Hub to enable frame verification.
   // hub: neynar({ apiKey: 'NEYNAR_FROG_FM' })
-  title: 'Frog Frame',
-})
+  title: "Frog Frame",
+});
 
-app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
+app.transaction("/placeBetFalse", (c) => {
+  console.log("gets false");
+  return c.contract({
+    abi: placeBetABI,
+    chainId: `eip155:1`,
+    functionName: "placeBet",
+    args: [false],
+    to: "0x7f3a5c4E4A33DBbb569B72094da4C40e64129523", // contract address
+    value: parseEther(".00004"),
+  });
+});
+
+app.transaction("/placeBetTrue", (c) => {
+  return c.contract({
+    abi: placeBetABI,
+    chainId: `eip155:${rootstockTestnet.id}`,
+    functionName: "placeBet",
+    args: [true],
+    to: "0x7f3a5c4E4A33DBbb569B72094da4C40e64129523", // contract address
+    value: parseEther(".0004"),
+  });
+});
+
+app.frame("/betPlaced", (c) => {
+  const { transactionId } = c;
   return c.res({
+    image: (
+      <div style={{ color: "white", display: "flex", fontSize: 60 }}>
+        Bet Placed Transaction ID: {transactionId}
+      </div>
+    ),
+  });
+});
+
+app.frame("/", (c) => {
+  const { buttonValue, inputText, status } = c;
+  return c.res({
+    action: "/betPlaced",
     image: (
       <div
         style={{
-          alignItems: 'center',
+          alignItems: "center",
           background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
-          backgroundSize: '100% 100%',
-          display: 'flex',
-          flexDirection: 'column',
-          flexWrap: 'nowrap',
-          height: '100%',
-          justifyContent: 'center',
-          textAlign: 'center',
-          width: '100%',
+            status === "response"
+              ? "linear-gradient(to right, #432889, #17101F)"
+              : "black",
+          backgroundSize: "100% 100%",
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          height: "100%",
+          justifyContent: "center",
+          textAlign: "center",
+          width: "100%",
         }}
       >
         <div
           style={{
-            color: 'white',
+            display: "flex",
+            color: "white",
             fontSize: 60,
-            fontStyle: 'normal',
-            letterSpacing: '-0.025em',
+            fontStyle: "normal",
+            letterSpacing: "-0.025em",
             lineHeight: 1.4,
             marginTop: 30,
-            padding: '0 120px',
-            whiteSpace: 'pre-wrap',
+            padding: "0 120px",
+            whiteSpace: "pre-wrap",
           }}
         >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+          <Image src="/h.png" />
         </div>
       </div>
     ),
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <Button.Transaction target="/placeBetFalse">
+        Less than cat 4
+      </Button.Transaction>,
+      <Button.Transaction target="/placeBetTrue">
+        Greater than cat 4
+      </Button.Transaction>,
+      status === "response" && <Button.Reset>Reset</Button.Reset>,
     ],
-  })
-})
+  });
+});
 
 // @ts-ignore
-const isEdgeFunction = typeof EdgeFunction !== 'undefined'
-const isProduction = isEdgeFunction || import.meta.env?.MODE !== 'development'
-devtools(app, isProduction ? { assetsPath: '/.frog' } : { serveStatic })
+const isEdgeFunction = typeof EdgeFunction !== "undefined";
+const isProduction = isEdgeFunction || import.meta.env?.MODE !== "development";
+devtools(app, isProduction ? { assetsPath: "/.frog" } : { serveStatic });
 
-export const GET = handle(app)
-export const POST = handle(app)
+export const GET = handle(app);
+export const POST = handle(app);
